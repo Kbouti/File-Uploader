b49f0296288092afdee1be434a6f973f4d88af9d
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-23 13:53:44 -0700

    Add session model to prisma schema and install prisma-session-store

--- app.js
+++ app.js
@@ -13,24 +13,40 @@ app.use(express.static("public"));
 app.set("views", __dirname);
 app.set("view engine", "pug");
 
-
-// Secret copied from setup tutorial is "cats". I changed this and set it in .env
-// Not sure if this will break things....
 const secret = process.env.SECRET;
-
 app.use(session({ secret, resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+// ******************************************************************************************
+// From prisma session store setup:
 
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+// ******************************************************************************************
 
 // const { PrismaClient } = require('@prisma/client')
 
 // const prisma = new PrismaClient()
 // // use `prisma` in your application to read and write data in your DB
-
-
-
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
 
