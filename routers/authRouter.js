8c0ef09acf4d253798645186345ce3245114c756
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-27 14:50:56 -0700

    Render logout button instead of signUp/logIn buttons when signed in

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -30,6 +30,7 @@ passport.use(
       if (!user) {
         console.log(`No user found with username: ${username}`);
         return done(null, false, { message: "Incorrect username" });
+        // I'd really like to know how to render the above message^^^ It's stored in the session, yes??
       }
       if (user.password !== password) {
         console.log(`User found with username: ${username}`);
@@ -49,5 +50,9 @@ authRouter.post(
     failureRedirect: "/logIn",
   })
 );
+
+
+// Need logout route
+
 
 module.exports = authRouter;
