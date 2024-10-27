dd377d6991677c483bdcd3be4713706afe76a517
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-27 14:17:45 -0700

    Create authRouter and begin refactor

---
+++ routers/authRouter.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const authRouter = Router();
+
+
+
+
+
+
+
+
+
+
+module.exports = authRouter;
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,7 +3,24 @@ const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 const userController = require("../controllers/userController");
 
-indexRouter.get("/", indexController.getIndex);
+
+/* GET home page. */
+indexRouter.get('/', function(req, res, next) {
+    if (!req.user) { return res.render("./views/pages/home", { title: "File Uploader" }); }
+    next();
+  }, function(req, res, next) {
+    res.locals.filter = null;
+    res.render("./views/pages/home", { title: "File Uploader", user: req.user });
+  });
+
+// ***** Refactored *****
+// Now we're getting our index route here, but we ar egoing to try to move authorization routes to routers/authRouter file
+
+
+
+
+
+
 
 indexRouter.get("/signUp", userController.getSignUp)
 
