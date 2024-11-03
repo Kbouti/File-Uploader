6effde02fa3cc126250a3d9308d8772ee976b37d
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-11-02 22:31:32 -0700

    Attempt to create folder with prisma

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -16,16 +16,40 @@ exports.createFolder = async (req, res, next) => {
   console.log(`Create folder controller function called`);
   console.log(`req.body.folderName: ${req.body.folderName}`);
 
-  if (req.body.folderName === "MAIN" || req.body.folderName === req.user.username + "_main") {
-    res.render("./views/pages/home", {title: "Failure", message: "Cannot create folder. This name is reserved for your main folder."
-    })
+  if (
+    req.body.folderName === "MAIN" ||
+    req.body.folderName === req.user.username + "_main"
+  ) {
+    res.render("./views/pages/home", {
+      title: "Failure",
+      message:
+        "Cannot create folder. This name is reserved for your main folder.",
+    });
     return;
   }
-  // Next we'll want to create a new folder in our database. 
+  // Next we'll want to create a new folder in our database. Neither of these attempts worked....
+  // We need to figure out how to create the folder in prisma
+
+  //   await prisma.user.upsert({
+  //     where: {
+  //       id: req.user.id,
+  //     },
+  //     create: {
+  //       Folder: {
+  //         base: false,
+  //         name: req.body.folderName,
+  //       },
+  //     },
+  //   });
+
+  //   await prisma.folder.create({data: {
+  //     owner: req.user,
+  //     base: false,
+  //     name: req.body.folderName
+  //   }})
 
   next();
 };
-
 
 // We should be able to rename the files that are uploaded as well as direct them to the database we want to
 // https://www.youtube.com/watch?v=i8yxx6V9UdM
