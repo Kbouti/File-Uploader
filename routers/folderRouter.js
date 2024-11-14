a357e1f24495c543cdf8b58c185be8af64ff2ae0
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-11-13 18:00:29 -0800

    Attempt to add folder id to crud operation parameters

--- routers/folderRouter.js
+++ routers/folderRouter.js
@@ -2,28 +2,40 @@ const { Router } = require("express");
 const folderRouter = Router();
 const folderController = require("../controllers/folderController");
 
-
-folderRouter.get("/newFolder",  (req, res) => {
+folderRouter.get("/newFolder", (req, res) => {
   console.log("newFolder get route reached");
   res.render("./views/pages/newFolder", { title: "New Folder" });
 });
 
-folderRouter.post("/newFolder", folderController.createFolder, async (req, res) => {
-  res.render("./views/pages/home", {title: "Success!", message: "Created new folder", user: req.user});
+folderRouter.post(
+  "/newFolder",
+  folderController.createFolder,
+  async (req, res) => {
+    res.render("./views/pages/home", {
+      title: "Success!",
+      message: "Created new folder",
+      user: req.user,
+    });
+  }
+);
+
+folderRouter.get("/openFolder", (req, res) => {
+  console.log("openFolder get route reached");
+  // res.render("./views/pages/openFolder", { title: "New Folder" });
+
+  console.log(`req.params: ${JSON.stringify(req.params)}`);
+  // This is an empty object, trying to get folder id
+
+  // console.log(`json(req): ${JSON.stringify(req)}`)
+  // This^^ gets error. Below gets [object Object]
+  console.log(`req: ${req}`);
+  res.send("open folder");
 });
 
-folderRouter.get("/openFolder",  (req, res) => {
-    console.log("openFolder get route reached");
-    // res.render("./views/pages/openFolder", { title: "New Folder" });
-    res.send("open folder")
-
-  });
-
-  folderRouter.get("/editFolder",  (req, res) => {
-    console.log("editFolder get route reached");
-    // res.render("./views/pages/openFolder", { title: "New Folder" });
-    res.send("edit folder")
-  });
-
+folderRouter.get("/editFolder", (req, res) => {
+  console.log("editFolder get route reached");
+  // res.render("./views/pages/openFolder", { title: "New Folder" });
+  res.send("edit folder");
+});
 
 module.exports = folderRouter;
